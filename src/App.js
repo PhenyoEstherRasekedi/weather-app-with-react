49811745deca023faf50f3fed3d7c81288c51dff
PhenyoEstherRasekedi <155717861+PhenyoEstherRasekedi@users.noreply.github.com>
2024-04-30 20:38:33 +0200

    created Weather.js and Weather.css

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
-      <footer>
+      <div className="Container">
         {" "}
-        This project was coded by{" "}
-        <a href="https://phenyorasekedi.netlify.app/">Phenyo Rasekedi </a>
-        and is{" "}
-        <a
-          href="https://github.com/PhenyoEstherRasekedi/weather-app-with-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on github
-        </a>
-      </footer>
+        <Weather />
+        <footer>
+          {" "}
+          This project was coded by{" "}
+          <a href="https://phenyorasekedi.netlify.app/">Phenyo Rasekedi </a>
+          and is{" "}
+          <a
+            href="https://github.com/PhenyoEstherRasekedi/weather-app-with-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            {" "}
+            <input
+              type="text"
+              placeholder="Enter a city.."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>Gaborone</h1>
+      <ul>
+        <li>Wednesday 07:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          18℃
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
+            alt="Clear with periodic clouds"
+          />
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 72%</li>
+            <li>Wiind: 13 km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
