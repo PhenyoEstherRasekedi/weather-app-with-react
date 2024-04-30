35e11ba386b95cb6003b52df42a6182194799c90
PhenyoEstherRasekedi <155717861+PhenyoEstherRasekedi@users.noreply.github.com>
2024-04-30 19:01:50 +0200

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <a href="https://github.com/PhenyoEstherRasekedi/weather-app-with-react">
+        Open sourced on github
+      </a>
+    </div>
+  );
 }
