cb4c703dec9f7a9b200311524668688dbe92ca6e
PhenyoEstherRasekedi <155717861+PhenyoEstherRasekedi@users.noreply.github.com>
2024-04-30 19:11:42 +0200

    Added GitHub Repo URL

--- src/App.js
+++ src/App.js
@@ -3,10 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <a href="https://github.com/PhenyoEstherRasekedi/weather-app-with-react">
-        Open sourced on github
-      </a>
+      <h1> Weather App</h1>
+      <footer>
+        {" "}
+        This project was coded by{" "}
+        <a href="https://phenyorasekedi.netlify.app/">Phenyo Rasekedi </a>
+        and is{" "}
+        <a
+          href="https://github.com/PhenyoEstherRasekedi/weather-app-with-react"
+          target="_blank"
+        >
+          open-sourced on github
+        </a>
+      </footer>
     </div>
   );
 }
