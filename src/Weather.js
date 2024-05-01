0d857b96c91f6eea5d2122a3df7f05660d8c02c7
PhenyoEstherRasekedi <155717861+PhenyoEstherRasekedi@users.noreply.github.com>
2024-05-01 21:48:36 +0200

    updated Weather.css

--- src/Weather.js
+++ src/Weather.js
@@ -12,25 +12,34 @@ export default function Weather() {
               type="text"
               placeholder="Enter a city.."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
+
       <h1>Gaborone</h1>
       <ul>
         <li>Wednesday 07:00</li>
         <li>Mostly Cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          18℃
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
             alt="Clear with periodic clouds"
+            className="float- mb-4"
           />
+
+          <span className="temperature">18</span>
+          <span className="unit">℃</span>
         </div>
         <div className="col-6">
           <ul>
